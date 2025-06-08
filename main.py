28022d4c4f1f5580af54daf45d4662155d5bbe90
Adam0x59 <4dam.5.m@gmail.com>
2025-06-08 21:02:39 +0100

    Added user input and input error handling.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -7,8 +8,14 @@ api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
+try:
+    prompt = sys.argv[1]
+except IndexError:
+    print('Please provide a prompt message.\nUsage: ./main.py <"Prompt message">')
+    sys.exit(1)
+
 response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    model='gemini-2.0-flash-001', contents=f"{prompt}"
 )
 print(response.text)
 print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
